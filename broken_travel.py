a8fce0a5ba8ddd90a1972ebe1f97e6fc990fe19f
monish114 <monigaju11@gmail.com>
2022-12-05 14:08:32 +0400

    commit

--- broken_travel.py
+++ broken_travel.py
@@ -4,7 +4,7 @@
 # year of origin (e.g., year = 2000)
 # and greet our strange visitor with a different message
 # if he is from the distant past (before 1900),
-# the present era (1900-2020) or from the far future (beyond 2020).
+# the present era (1900-2020) or from the far future (beyond 2020)
 
 year = int(input("Greetings! What is your year of origin?")) 
 
